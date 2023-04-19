fddfc5a80e358bd7751b148bb5b41b1fbcfb2986
RafaBRZL <rafaelbrizola05@gmail.com>
2023-04-19 00:48:06 -0300

    atualização aula 8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,14 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { FormBuilder } from '@angular/forms';
+//Junto aos demais imports de app.module.ts
+import { IonicStorageModule } from '@ionic/storage-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
-  bootstrap: [AppComponent],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, IonicStorageModule.forRoot()],
+  providers: [FormBuilder,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
